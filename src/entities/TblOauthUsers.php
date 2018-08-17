438e23b0704167343ebf97f0b3b2d3c6cdeba1f4
Dimitris Andritsakis <andritsakisd@gmail.com>
2018-08-17 18:01:02 +0300

    entities update

---
+++ src/entities/TblOauthUsers.php
@@ -0,0 +1,97 @@
+<?php
+
+
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * TblOauthUsers
+ *
+ * @ORM\Table(name="tbl_oauth_users", uniqueConstraints={@ORM\UniqueConstraint(name="UNIQ_93804FF8E7927C74", columns={"email"})}, indexes={@ORM\Index(name="email_index", columns={"email"})})
+ * @ORM\Entity
+ */
+class TblOauthUsers
+{
+    /**
+     * @var integer
+     *
+     * @ORM\Column(name="id", type="integer", nullable=false)
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="IDENTITY")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="email", type="string", length=50, nullable=false)
+     */
+    private $email;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="password", type="string", length=255, nullable=false)
+     */
+    private $password;
+
+
+
+    /**
+     * Get id
+     *
+     * @return integer
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set email
+     *
+     * @param string $email
+     *
+     * @return TblOauthUsers
+     */
+    public function setEmail($email)
+    {
+        $this->email = $email;
+
+        return $this;
+    }
+
+    /**
+     * Get email
+     *
+     * @return string
+     */
+    public function getEmail()
+    {
+        return $this->email;
+    }
+
+    /**
+     * Set password
+     *
+     * @param string $password
+     *
+     * @return TblOauthUsers
+     */
+    public function setPassword($password)
+    {
+        $this->password = $password;
+
+        return $this;
+    }
+
+    /**
+     * Get password
+     *
+     * @return string
+     */
+    public function getPassword()
+    {
+        return $this->password;
+    }
+}
